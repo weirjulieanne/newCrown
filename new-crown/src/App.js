8a5eaf8c0399a8bc8303a8969bfd88c8edeca01b
weirjulieanne <85594164+weirjulieanne@users.noreply.github.com>
2022-03-15 09:53:35 -0600

    first commit

---
+++ new-crown/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import HomePage from "./HomePage";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
